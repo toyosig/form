1cbf9b364f0fea469df439f2466162d803896527
toyosig <gadesanya9907@gmail.com>
2026-07-08 23:12:17 +0100

    Fix SSL connection to Supabase via connection pooler

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('sslmode=require')
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 pool.connect()
