8b2e638f83d7aa770be01d2dadad543c7888e056
toyosig <gadesanya9907@gmail.com>
2026-07-08 23:13:07 +0100

    Force SSL for Supabase pooler connection

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('sslmode=require')
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.connect()
